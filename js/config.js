968426f4e9e5dde058e46619703668dbc86e8db9
nerazouri <74005581+nerazouri@users.noreply.github.com>
2020-11-06 10:54:40 +0000

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_bDuFp0MQV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4t1m2u735stpgq99urs318imts', // e.g. 25ddkmj4v6hfsfv$
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaw$
+    }
+};
